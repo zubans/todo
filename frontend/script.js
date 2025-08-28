f1a1708c756e26c5ecbed88f3b214faed03076b0
zubans <zubans@mail.ru>
2025-08-28 12:03:08 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const API_BASE_URL = 'http://localhost:8080'; // Базовый URL API
+  const API_BASE_URL = 'http://192.168.1.118:8080'; // Базовый URL API
   const form = document.getElementById('todo-form');
   const input = document.getElementById('todo-input');
   const list = document.getElementById('todo-list');
